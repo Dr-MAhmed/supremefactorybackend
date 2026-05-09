f9c7a4fe222a95fc0ff2841e566ddf90b75f08de
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2026-05-10 02:10:22 +0500

    cors errors

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ['https://supremefactory.vercel.app', 'http://localhost:5173', 'http://localhost:4173'],
+    origin: ['https://supremefactory.vercel.app', 'http://localhost:5173', 'http://localhost:4173', 'https://supremefactorybackend.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
