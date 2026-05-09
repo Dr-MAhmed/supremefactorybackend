c44abc3a200ba367c14e0d6b8c49cb88531d50bf
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2026-05-10 00:42:42 +0500

    fixing errors

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import serverless from "serverless-http";
-import app from "../app";
-
-const handler = serverless(app);
-
-export default async (req: any, res: any) => {
-  const result = await handler(req, res);
-  return result;
-};
