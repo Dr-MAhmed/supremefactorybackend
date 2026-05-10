b37c437f93f447eae1c7f749fb15ba582de04c3a
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2026-05-10 15:13:16 +0500

    fix: CORS errors, remove helmet SPA Rewrites

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,14 @@ app.use((_req, res, next) => {
   next();
 });
 
+app.options('*', (_req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://supremefactory.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.status(204).end();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
