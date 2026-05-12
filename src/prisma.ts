5a8ed718a5f36275143a903e4a2eae0ac88875d5
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2026-05-12 17:57:42 +0500

    fixing prisma ORM

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,17 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
 const createPrismaClient = () => {
-  const url = process.env.DATABASE_URL?.replace('&channel_binding=require', '');
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  });
   return new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-    datasources: {
-      db: { url }
-    },
-    errorFormat: 'pretty'
   });
 };
 
